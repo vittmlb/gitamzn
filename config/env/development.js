155b514322fba44b24126d242ec7782310f236f8
Vittorio <vittorio.cadastro@gmail.com>
2016-12-18 18:00:54 -0200

    pronto para deploy

--- config/env/development.js
+++ config/env/development.js
@@ -4,6 +4,6 @@
 module.exports = {
     sessionSecret: 'mSecret',
     // db: 'mongodb://heroku_bqns2x5j:heroku_bqns2x5j@ds133158.mlab.com:33158/heroku_bqns2x5j'
-    // db: 'mongodb://vmlb:Utld2287@ds053186.mlab.com:53186/dbconcorrentes'
-    db: 'mongodb://localhost/impAmazon'
+    db: 'mongodb://vmlb:Utld2287@ds053186.mlab.com:53186/dbconcorrentes'
+    // db: 'mongodb://localhost/impAmazon'
 };
